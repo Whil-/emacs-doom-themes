9fa1a129bbb0ea10098690b67b8e40175ec9bb4b
Gustav Wikström <gustav@whil.se>
2020-06-06 22:12:24 +0200

    Remove org-block background extensions
    They clutter the files!

--- doom-themes-base.el
+++ doom-themes-base.el
@@ -1236,9 +1236,9 @@
 
     ;; org-mode
     (org-archived                 :foreground doc-comments)
-    (org-block                    :background base3 :extend t)
-    (org-block-background         :background base3 :extend t)
-    (org-block-begin-line         :foreground comments :background base3 :extend t)
+    (org-block                    :background base3)
+    (org-block-background         :background base3)
+    (org-block-begin-line         :foreground comments :background base3)
     (org-block-end-line           :inherit 'org-block-begin-line)
     (org-checkbox                 :inherit 'org-todo)
     (org-checkbox-statistics-done :inherit 'org-done)
